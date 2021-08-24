4c85cdb755a6b9bc8a92f0b08dc617217ab2f5aa
kryptoniteX <89017901+kryptoniteX@users.noreply.github.com>
2021-08-24 17:33:46 +0530

    X01BD - Initial Revenge OS Bringup

---
+++ revengeos_X01BD.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Superior stuff
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := revengeos_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Asus Zenfone Max Pro M2
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
