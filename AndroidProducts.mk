31e3dab034bd6f8ce848495b6a0d994b33078516
kryptoniteX <89017901+kryptoniteX@users.noreply.github.com>
2021-08-24 17:32:33 +0530

    X01BD - Initial Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/revengeos_X01BD.mk
+
+COMMON_LUNCH_CHOICES := \
+    revengeos_X01BD-user \
+    revengeos_X01BD-userdebug \
+    revengeos_X01BD-eng
